b530d65474ba5743a8ab71a19f2175306c887720
matthew graham <mdg149@gmail.com>
2026-09-23 20:20:49 -0400

    add favicon png files
    remove the old favicon.svg file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,16 @@
 	import { page } from '$app/state';
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon32 from '$lib/assets/favicon-32x32.png';
 	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
+	<link rel="icon" href={favicon32} sizes="32x32" type="image/png" />
+</svelte:head>
 <div class="flex min-h-screen flex-col bg-stone-200 text-stone-900">
 	<div class="flex-1">
 		{@render children()}
